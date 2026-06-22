7591bfb4d6d8c1ca0abddf6cc40cdcae2c4097cc
Piyushkumar-20 <pk6686742@gmail.com>
2026-06-22 19:19:46 +0530

    feat: Add CORS support to backend and remove unused frontend files

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser"
 import express from "express";
 import authRoute from "./modules/auth/auth.routes.js";
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
