532f0812258e80b0eaec44b176c4594a2accb947
Piyushkumar-20 <pk6686742@gmail.com>
2026-06-23 17:00:38 +0530

    feat: Add CORS middleware to enable cross-origin requests from the frontend, enhancing API accessibility.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,14 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 import authRoute from "./modules/auth/auth.routes.js";
 import tripRoute from "./modules/trips/trip.routes.js";
 
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
